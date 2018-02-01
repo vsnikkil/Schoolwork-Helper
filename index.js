68968c73df2431ebe7dd514168e726d27c26e2a8
L1lith <lily@lillith.pw>
2018-01-31 18:59:35 -0700

    server bug fixes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const webpack = require('webpack');
 const minimist = require('minimist');
-
-const developmentConfig = require('./webpack.dev.js');
-const productionConfig = require('./webpack.prod.js');
 const startServer = require('./server');
 
 const args = minimist(process.argv.slice(2));
