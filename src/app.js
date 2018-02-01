81b668b8d47515649e1c4c187268464e53242059
L1lith <lily@lillith.pw>
2018-01-31 19:46:11 -0700

    fix merge issue

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
-<<<<<<< Updated upstream
 const path = require('path');
 
 function fromHere(filePath){
   return path.resolve(__dirname,filePath);
 }
-=======
-var path = require ('path');
->>>>>>> Stashed changes
 // set the view engine to ejs
 
 
