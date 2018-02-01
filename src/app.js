50f2eb2136ef11776bfcdf9ae98e63c41de796c6
BrandonDalbec <dalbecb@gmail.com>
2018-01-31 18:43:46 -0800

    .

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const ejs = require('ejs');
+<<<<<<< Updated upstream
 const path = require('path');
 
 function fromHere(filePath){
   return path.resolve(__dirname,filePath);
 }
+=======
+var path = require ('path');
+>>>>>>> Stashed changes
 // set the view engine to ejs
+
 
 module.exports = function(app){
   app.set('view engine', 'ejs');
